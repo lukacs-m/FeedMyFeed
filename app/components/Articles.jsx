77fd18412f0e0bd23aeeca173dbb6ca1fae987ec
martin <martin.luk@epitech.eu>
2016-12-18 18:46:43 +0100

    start of the display of user articles

--- app/components/Articles.jsx
+++ app/components/Articles.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NewsItem } from 'NewsItem';
 
 export class Articles extends Component {
     render () {
-        let test = this.props;
-        console.log("props in article", test);
+        let { articles } = this.props;
+
+        let renderArticlesItems = () => {
+            return articles.map((articlesItem) => {
+                let displayArticle = articlesItem.articleContent;
+                console.log("articels items", displayArticle);
+                return (
+                    <NewsItem  key={ articlesItem.id.toString()} { ...displayArticle } />
+                );
+            });
+        };
+
         return (
             <div>
-            coucou tu es sur feed
+                <h1 className="page-title">Your saved articles</h1>
+                {/*<div className="row small-up-1 medium-up-2 large-up-2">*/}
+                    <div className="column small-centered small-12" >
+                        { renderArticlesItems() }
+                    </div>
+                 {/*</div>*/}
             </div>
         );
     }
